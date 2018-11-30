361af55133645e0576e26239c54e670e85a3b30c
Usama <learnbeginners@gmail.com>
2018-11-30 12:10:45 +0500

    Seeding Database

---
+++ Migrations/20181130070857_SeedDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make3')");
+             // These queries are because when we downgrade the migration, records will be deleted. And primary key has identity column.
+            // So it will not work if foreign keys contain hard coded numbers here in the query. So we make it dynamic here.
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelA', (SELECT MakeId FROM Makes WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelB', (SELECT MakeId FROM Makes WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelC', (SELECT MakeId FROM Makes WHERE Name = 'Make1'))");
+             migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make2-ModelA', (SELECT MakeId FROM Makes WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make2-ModelB', (SELECT MakeId FROM Makes WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make2-ModelC', (SELECT MakeId FROM Makes WHERE Name = 'Make2'))");
+             migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make3-ModelA', (SELECT MakeId FROM Makes WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make3-ModelB', (SELECT MakeId FROM Makes WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make3-ModelC', (SELECT MakeId FROM Makes WHERE Name = 'Make3'))");
+        
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Proper Implementation to delete the records is
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name IN ('Make1', 'Make2', 'Make3')");
+            // Actually when we delete Makes, associated Models will automatically deleted
+             // migrationBuilder.Sql("DELETE FROM Models");
+        }
+    }
+}
