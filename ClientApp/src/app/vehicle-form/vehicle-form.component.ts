db24b4b85b916fd246dc4a018727008f8a0ff047
Usama <learnbeginners@gmail.com>
2018-12-03 13:00:54 +0500

    Binding TextBox

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -10,7 +10,8 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   vehicle: any = {
-    features: []
+    features: [],
+    contact: {}
   };
   features: any;
 
